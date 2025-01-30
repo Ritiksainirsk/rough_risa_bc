6990fdb49d6fcb18081c33a74bba1c0e618681d0
unknown <ritiksainirsk@gmail.com>
2025-01-30 14:31:43 +0530

    second  commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.post('/',(req,res)=>{
-    res.send('hello world')
+    res.send('hello world what up jhsdjf  sdjf jsd fj ')
 })
 
 app.listen(3000,()=>{
